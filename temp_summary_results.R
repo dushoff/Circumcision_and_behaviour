31668127718b1ad5349d41dbf90efdcebc05f630
wzmli <wzmichael.li@gmail.com>
2016-06-29 14:39:55 -0400

    JD will not like this, will fix later when he comes back

---
+++ temp_summary_results.R
@@ -0,0 +1,25 @@
+library(ordinal)
+
+##summary results
+## Partner Year Status
+load(".partnerYearStatus.RData")
+summary(mod)
+
+
+
+## Condom Recency
+load(".condomRecency.RData")
+summary(mod)
+
+
+## Partner Year Recency
+load(".partnerYearRecency.RData")
+summary(mod)
+
+
+## Partner Life Recency
+load(".partnerYearRecency.RData")
+summary(mod)
+
+
+
